6bb600fc2ecc54d043ebc35489a194591231a0b3
februarysea <857254373@qq.com>
2018-06-27 18:40:43 +0800

    day12

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+func greet(_ person: String) -> String {
+    let greeting = "Hello," + person + "!"
+    return greeting
+}
+
+print(greet(Susan))
