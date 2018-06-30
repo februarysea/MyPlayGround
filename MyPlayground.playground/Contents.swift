8ec04e4a10dd05df09a9d98b00609b2de27f60c0
februarysea <857254373@qq.com>
2018-06-30 14:40:59 +0800

    day15

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,9 +2,19 @@
 
 import Foundation
 
-func greet(_ person: String) -> String {
-    let greeting = "Hello," + person + "!"
-    return greeting
+func minMax(array: [Int]) -> (min:Int, max: Int)? {
+    if array.isEmpty {
+        return nil
+    }
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        }
+        else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
 }
-
-print(greet(Susan))
