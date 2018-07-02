db15b5b4b5bfe486a08697b91117e9adfa7e8b44
februarysea <857254373@qq.com>
2018-07-02 11:10:39 +0800

    day17

---
+++ day16.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+//Closures（闭包）
+
+
+
---
+++ day17.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+//Closures（闭包）
+func backward(_ s1: String, _ s2:String) -> Bool
+
