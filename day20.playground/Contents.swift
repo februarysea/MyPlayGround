95bd18212a66a39f2080bffbef1d6d1be0fede47
februarysea <857254373@qq.com>
2018-08-21 09:52:05 +0800

    day20

--- day20.playground/Contents.swift
+++ day20.playground/Contents.swift
@@ -9,4 +9,32 @@ let digitNames = [
 
 let numbers = [16,58, 510]
 
+let strings = numbers.map {
+    (number) -> String in
+    var number = number
+    var output = ""
+    repeat {
+        output = digitNames[number % 10]! + output
+        number /= 10
+    }while number > 0
+    return output
+}
+
+func makeIncrementer(forIncrement amount: Int) -> () -> Int {
+    var runningToal = 0
+    func incrementer() -> Int {
+        runningToal += amount
+        return runningToal
+    }
+    return incrementer
+}
+
+let incrementByTen = makeIncrementer(forIncrement: 10)
+incrementByTen()
+incrementByTen()
+incrementByTen()
+
+
+
+
 
